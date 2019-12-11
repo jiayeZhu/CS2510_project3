26cf9f5950854bb26993550258ec255d93b621d1
jiayeZhu <1352953@tongji.edu.cn>
2019-12-11 18:29:19 -0500

    add hdfs cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
-rm -rf output
-rm -rf cell2pointLUT
-rm -rf point2cellLUT
-rm -rf idMapping
+hadoop fs -rm -rf output
+hadoop fs -rm -rf cell2pointLUT
+hadoop fs -rm -rf point2cellLUT
+hadoop fs -rm -rf idMapping
 rm project3.jar
 rm src/main/java/*.class
