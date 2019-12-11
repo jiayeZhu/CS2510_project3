1913eb6da8bc9c0080890b77650e425d615619f8
jiayeZhu <1352953@tongji.edu.cn>
2019-12-11 18:30:24 -0500

    fix script

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
-hadoop fs -rm -rf output
-hadoop fs -rm -rf cell2pointLUT
-hadoop fs -rm -rf point2cellLUT
-hadoop fs -rm -rf idMapping
+hadoop fs -rm -r output
+hadoop fs -rm -r cell2pointLUT
+hadoop fs -rm -r point2cellLUT
+hadoop fs -rm -r idMapping
 rm project3.jar
 rm src/main/java/*.class
